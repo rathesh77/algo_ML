97c7e5a3b09da76ec2128fae819cc99307c71df3
rathesh77 <rathesh.charles@outlook.fr>
2023-05-11 16:45:11 +0200

    wip

--- recommands.js
+++ recommands.js
@@ -57,38 +57,15 @@ const critics = {
 };
 
 function euclidian_distance(c1, c2) {
-  const c1Films = Object.keys(c1);
-  const c2Films = Object.keys(c2);
-
-  const films = Array.from(new Set([...c1Films, ...c2Films]));
-  const uniqs = [];
-
+  const films = Object.keys(c1);
+  const filmsC2 = Object.keys(c2);
+  let dist = 0;
   for (const film of films) {
-    if (!c1[film] || !c2[film]) {
-      continue;
-    }
-    uniqs.push(film);
-  }
-
-  const distances = [];
-  for (let i = 0; i < uniqs.length; i++) {
-    for (let j = i + 1; j < uniqs.length; j++) {
-      
-      const x1 = c1[uniqs[i]];
-      const y1 = c1[uniqs[j]];
-
-      const x2 = c2[uniqs[i]];
-      const y2 = c2[uniqs[j]];
-
-      const dist = Math.sqrt(
-        Math.abs((x2 - x1) ** 2) + Math.abs((y2 - y1) ** 2)
-      );
-      const normalized = 1 / (1 + dist);
-      distances.push(normalized);
+    if (film in filmsC2) {
+      dist += (c1[film] - c2[film]) ** 2;
     }
   }
-  let mean = distances.reduce((acc, val) => acc + val, 0) / distances.length;
-  return mean;
+  return 1 / (1 + Math.sqrt(dist));
 }
 
 function pearson_correlation(c1, c2) {
@@ -110,8 +87,8 @@ function pearson_correlation(c1, c2) {
 
   let sum_x_mult_y = 0;
 
-  let sum_x_s_squared = uniqs.reduce((acc, val) => acc + (c1[val] ** 2), 0);
-  let sum_y_s_squared = uniqs.reduce((acc, val) => acc + (c2[val] ** 2), 0);
+  let sum_x_s_squared = uniqs.reduce((acc, val) => acc + c1[val] ** 2, 0);
+  let sum_y_s_squared = uniqs.reduce((acc, val) => acc + c2[val] ** 2, 0);
 
   for (const film of uniqs) {
     const x = c1[film];
@@ -121,16 +98,81 @@ function pearson_correlation(c1, c2) {
   }
 
   return (
-    ((uniqs.length * sum_x_mult_y) - (xs * ys)) /
+    (uniqs.length * sum_x_mult_y - xs * ys) /
     Math.sqrt(
-      ((uniqs.length * sum_x_s_squared) - (xs ** 2)) *
-        ((uniqs.length * sum_y_s_squared) - (ys ** 2))
+      (uniqs.length * sum_x_s_squared - xs ** 2) *
+        (uniqs.length * sum_y_s_squared - ys ** 2)
     )
   );
 }
 
-console.log(euclidian_distance(critics["Lisa Rose"], critics["Michael Phillips"]));
-console.log(euclidian_distance(critics["Lisa Rose"], critics["Gene Seymour"]));
+function recommandation(toby) {
+  let similarities = {};
 
-console.log(pearson_correlation(critics["Lisa Rose"], critics["Michael Phillips"]));
-console.log(pearson_correlation(critics["Lisa Rose"], critics["Gene Seymour"]));
+  const films = Array.from(
+    new Set(
+      Object.keys(critics)
+        .map((p) => critics[p])
+        .map((f) => Object.keys(f))
+        .reduce((a, v) => a.concat(v), [])
+    )
+  ).filter((f) => !critics[toby][f]);
+  let persons = [];
+  for (const person of Object.keys(critics)) {
+    if (
+      Object.keys(critics[person]).filter((f) => films.find((e) => e == f))
+        .length > 0
+    ) {
+      persons.push(person);
+    }
+  }
+
+  for (const p of persons) {
+    similarities[p] = pearson_correlation(critics[toby], critics[p]);
+  }
+  let sims = [];
+  for (const person of Object.keys(similarities)) {
+    sims.push([person, similarities[person]]);
+  }
+
+  sims = sims.sort(function (a, b) {
+    return b[1] - a[1];
+  });
+
+  sims = sims.slice(0, 5);
+  similarities = {};
+  for (let i = 0; i < sims.length; i++) {
+    similarities[sims[i][0]] = sims[i][1];
+  }
+  persons = Object.keys(similarities);
+
+  const results = [];
+  for (const film of films) {
+    let s_dot_film_total = 0;
+
+    for (const person of persons) {
+      const s_dot_film = similarities[person] * critics[person][film];
+      if (critics[person][film] == null) continue;
+      s_dot_film_total += s_dot_film;
+    }
+    let sim_dot_sum = 0;
+    for (const p of Object.keys(similarities)) {
+      if (critics[p][film] == null) continue;
+
+      sim_dot_sum += similarities[p];
+    }
+
+    const total_sim_sum = s_dot_film_total / sim_dot_sum;
+    results.push([film, total_sim_sum]);
+  }
+
+  return results.sort((a, b) => b[1] - a[1])[0];
+}
+
+//console.log(euclidian_distance(critics["Lisa Rose"], critics["Gene Seymour"]));
+//console.log(euclidian_distance(critics["Lisa Rose"], critics["Michael Phillips"]));
+
+//console.log(pearson_correlation(critics["Lisa Rose"], critics["Gene Seymour"]));
+//console.log(pearson_correlation(critics["Lisa Rose"], critics["Michael Phillips"]));
+
+console.log(recommandation("Toby"));
