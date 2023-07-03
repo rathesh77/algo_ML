437a11fcefd912255e21ae3546cf40f1d6b84f0f
rathesh77 <rathesh.charles@outlook.fr>
2023-07-03 12:23:33 +0200

    fix generateSolutions function

--- probleme du voyageur/main.js
+++ probleme du voyageur/main.js
@@ -21,61 +21,79 @@ function parseXlsx(filename) {
   return jsonData;
 }
 
-async function parseXml(filename) {
-  if (!fs.existsSync(filename)) {
-    console.log("file " + filename + " doesnt exist");
-    return -1;
+async function parseXml() {
+
+  let flights = {}
+  for (const date of ['07-26', '07-27', '08-03', '08-04']) {
+    flights[date] = {}
+    for (const vol of fs.readdirSync(ROOT + "/" + date)) {
+      const xmlData = fs.readFileSync(ROOT + "/" + date + "/" + vol, "utf8");
+      const result = await parseStringPromisified(xmlData);
+      flights[date][vol] = result["flights"]["flight"]
+    }
   }
-  const xmlData = fs.readFileSync(filename, "utf8");
-  const result = await parseStringPromisified(xmlData);
-  return result;
+
+  return flights;
 }
 
 function cout(s1, s2) {}
 
 async function generateSolutions() {
   const out = { aller: {}, retour: {} };
-  let secondesDate27juillet18h = new Date("2010-07-27T18:00:00").getTime()/ 1000;
+  let secondesDate27juillet18h = new Date("2010-07-27T18:00:00").getTime() / 1000;
   let solutions = []
-  const nbSolutions = 40;
-  for (let i = 0; i < nbSolutions ;i ++){
+  const nbSolutions = 4000;
+  for (let k = 0; k < nbSolutions; k++) {
     solutions.push({})
-  let coutTotal = 0
-  for (const date of fs.readdirSync(ROOT)) {
-    for (const vol of fs.readdirSync(ROOT + "/" + date)) {
-      const result = await parseXml(ROOT + "/" + date + "/" + vol);
-      let type = null
-      if (date.substring(0, 2) == "07") {
-        type = 'aller'
-      } else {
-        type = 'retour'
+    let coutTotal = 0
+    for (let i = 0; i < 9; i++) {
+      const date = Math.random() > 0.5 ? '07-26' : '07-27'
+      let type = 'aller'
+      const vol = fs.readdirSync(ROOT + "/" + date)[i]
+      const vols = flights[date][vol];
+      if (!solutions[k][type]) {
+        solutions[k][type] = {}
+        solutions[k][type][vol] = []
+      } else if (!solutions[k][type][vol]) {
+        solutions[k][type][vol] = []
       }
-      const vols = result["flights"]["flight"];
-      if (!solutions[i][type]) {
-        solutions[i][type] = {}
-        solutions[i][type][vol] = []
-      } else if (!solutions[i][type][vol]) {
-        solutions[i][type][vol] = []
-      }
-      let cout = +vols[i].price.reduce((a, v) => +a + +v, 0);
-      let secondesDateDepart = new Date(vols[i].depart[0]).getTime() / 1000;
-      let secondesDateArrivee = new Date(vols[i].arrive[0]).getTime() / 1000;
-      let tempsDattenteAeroport = Math.abs(secondesDate27juillet18h - secondesDateArrivee) 
+      let randVol = randomIntFromInterval(0, vols.length - 1)
+      let cout = +vols[randVol].price.reduce((a, v) => +a + +v, 0);
+      let secondesDateDepart = new Date(vols[randVol].depart[0]).getTime() / 1000;
+      let secondesDateArrivee = new Date(vols[randVol].arrive[0]).getTime() / 1000;
+      let tempsDattenteAeroport = Math.abs(secondesDate27juillet18h - secondesDateArrivee)
       let tempsVol = secondesDateArrivee - secondesDateDepart
-      coutTotal += cout +  ((tempsDattenteAeroport / 60) * 10)
-      solutions[i][type][vol].push({'vol':vols[randomIntFromInterval(0, vols.length -1)], tempsDattenteAeroport, cout, tempsVol})
-    
-      if (solutions[i][type][vol].length == 2) {
-        solutions[i][type][vol] = solutions[i][type][vol][randomIntFromInterval(0,solutions[i][type][vol].length-1)]
+      coutTotal += cout + ((tempsDattenteAeroport / 60) * 10)
+      solutions[k][type][vol] = ({ 'vol': vols[randVol], tempsDattenteAeroport, cout, tempsVol })
+
+
+    }
+    for (let i = 0; i < 9; i++) {
+      const date = Math.random() > 0.5 ? '08-03' : '08-04'
+      let type = 'retour'
+      const vol = fs.readdirSync(ROOT + "/" + date)[i]
+      const vols = flights[date][vol];
+      if (!solutions[k][type]) {
+        solutions[k][type] = {}
+        solutions[k][type][vol] = []
+      } else if (!solutions[k][type][vol]) {
+        solutions[k][type][vol] = []
       }
-      //console.log(result)
+      let randVol = randomIntFromInterval(0, vols.length - 1)
+      let cout = +vols[randVol].price.reduce((a, v) => +a + +v, 0);
+      let secondesDateDepart = new Date(vols[randVol].depart[0]).getTime() / 1000;
+      let secondesDateArrivee = new Date(vols[randVol].arrive[0]).getTime() / 1000;
+      let tempsDattenteAeroport = Math.abs(secondesDate27juillet18h - secondesDateArrivee)
+      let tempsVol = secondesDateArrivee - secondesDateDepart
+      coutTotal += cout + ((tempsDattenteAeroport / 60) * 10)
+      solutions[k][type][vol] = ({ 'vol': vols[randVol], tempsDattenteAeroport, cout, tempsVol })
+
+
     }
     
+    solutions[k]['coutTotal'] = coutTotal
   }
-  solutions[i]['coutTotal'] = coutTotal
-}
-  
-  console.log(solutions)
+
   return solutions
 }
 
@@ -113,13 +131,13 @@ async function simulatedAnnealing() {
   let highCost = solutions[0].coutTotal
   let lowCost = highCost
   let bestSolution = lowCost
-  for (let i = 1;i < solutions.length; i++) {
+  for (let i = 1;i < solutions.length && temperature > 0; i++) {
     if (solutions[i].coutTotal < lowCost) {
       const temp = lowCost
       lowCost = solutions[i].coutTotal
       highCost = temp
-      const probability = Math.pow(Math.exp(1), (-highCost-lowCost)/temperature )
-      if (probability > Math.random()) {
+      const probability = Math.pow(Math.exp(1), ((-highCost-lowCost)/temperature ))
+      if (probability > Math.random() * 2) {
         temperature *= cool
         bestSolution = lowCost
       }
@@ -128,6 +146,11 @@ async function simulatedAnnealing() {
   }
   return bestSolution
 }
-//generateSolutions();
+let flights = null
+async function main () {
+  flights = await parseXml()
+  await hillClimbing()
+  //await simulatedAnnealing()
+}
 //hillClimbing()
-simulatedAnnealing()
+main()
