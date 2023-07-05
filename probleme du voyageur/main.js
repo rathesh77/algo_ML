cf799dcf43b906527d1e5341ef75821c5d408a9a
rathesh77 <rathesh.charles@outlook.fr>
2023-07-05 11:36:01 +0200

    traduction des variables + correction des temps d'attentes dans l'aeroport

--- probleme du voyageur/main.js
+++ probleme du voyageur/main.js
@@ -6,33 +6,30 @@ const parseStringPromisified = util.promisify(parseString);
 
 const ROOT = "./ThirdParty/FlightData/2010";
 
-function randomIntFromInterval(min, max) { // min and max included 
+function entierAleatoireEntre(min, max) { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 async function parseXml() {
 
-  let flights = {}
+  let vols = {}
   for (const date of ['07-26', '07-27', '08-03', '08-04']) {
-    flights[date] = {}
+    vols[date] = {}
     for (const vol of fs.readdirSync(ROOT + "/" + date)) {
       const xmlData = fs.readFileSync(ROOT + "/" + date + "/" + vol, "utf8");
       const result = await parseStringPromisified(xmlData);
-      flights[date][vol] = result["flights"]["flight"]
+      vols[date][vol] = result["flights"]["flight"]
     }
   }
 
-  return flights;
+  return vols;
 }
 
-function cout(s1, s2) {}
-
-async function generateSolutions() {
+async function genererSolutions() {
   const out = { aller: {}, retour: {} };
-  let secondesDate27juillet18h = new Date("2010-07-27T18:00:00").getTime() / 1000;
   let solutions = []
   const nbSolutions = 4000;
-  const flights = await parseXml()
+  const volsXml = await parseXml()
 
   let maxArriveeAeroport = -Infinity
   for (let k = 0; k < nbSolutions; k++) {
@@ -43,29 +40,34 @@ async function generateSolutions() {
         const date = type == 'aller' ? (Math.random() > 0.5 ? '07-26' : '07-27') : (Math.random() > 0.5 ? '08-03' : '08-04')
 
         const nomVol = fs.readdirSync(ROOT + "/" + date)[i]
-        const vols = flights[date][nomVol];
+        const vols = volsXml[date][nomVol];
         if (!solutions[k][type]) {
           solutions[k][type] = {}
           solutions[k][type][nomVol] = []
         } else if (!solutions[k][type][nomVol]) {
           solutions[k][type][nomVol] = []
         }
-        let randVol = randomIntFromInterval(0, vols.length - 1)
+        let randVol = entierAleatoireEntre(0, vols.length - 1)
         let cout = +vols[randVol].price.reduce((a, v) => +a + +v, 0);        let secondesDateDepart = new Date(vols[randVol].depart[0]).getTime() / 1000;
         let secondesDateArrivee = new Date(vols[randVol].arrive[0]).getTime() / 1000;
         let tempsVol = secondesDateArrivee - secondesDateDepart
-        if (secondesDateArrivee > maxArriveeAeroport)
+        if (type == 'aller' && secondesDateArrivee > maxArriveeAeroport)
           maxArriveeAeroport = secondesDateArrivee
         coutTotal += cout
-        solutions[k][type][nomVol] = ({ 'vol': vols[randVol], cout, tempsVol, secondesDateArrivee })
+        solutions[k][type][nomVol] = ({ 'vol': vols[randVol], cout, tempsVol })
+        if (type == 'aller')
+          solutions[k][type][nomVol]['secondesDateArrivee'] = secondesDateArrivee
       }
     }
     solutions[k]['coutTotal'] = coutTotal
   }
   for (let i = 0; i < solutions.length; i++) {
-    for (let j = 0; j < solutions[i].aller.length; j++) {
-      solutions[i].aller[j].tempsDattenteAeroport = maxArriveeAeroport - solutions[i].aller[j].secondesDateArrivee
-      solutions[i].coutTotal += ((solutions[i].aller[j].tempsDattenteAeroport / 60) * 10)
+    const vols = Object.keys(solutions[i].aller)
+
+    for (let j = 0; j < vols.length; j++) {
+      solutions[i].aller[vols[j]].tempsDattenteAeroport = (maxArriveeAeroport - solutions[i].aller[vols[j]].secondesDateArrivee) / 1000
+      solutions[i].coutTotal += (solutions[i].aller[vols[j]].tempsDattenteAeroport / 60) >= 30 ? ((solutions[i].aller[vols[j]].tempsDattenteAeroport / 60) * 2) : 0
+      delete solutions[i].aller[vols[j]].secondesDateArrivee
     }
   }
 
@@ -77,8 +79,8 @@ async function hillClimbing() {
 
   const minSolutionsVoisines = function (solutions) {
     let min = {coutTotal: Infinity}
-    let a = randomIntFromInterval(0, solutions.length-2)
-    let b = randomIntFromInterval(a+1, solutions.length-1)
+    let a = entierAleatoireEntre(0, solutions.length-2)
+    let b = entierAleatoireEntre(a+1, solutions.length-1)
 
     for (const solution of solutions.slice(a, b + 1)) {
       if (solution.coutTotal < min.coutTotal) {
@@ -88,50 +90,58 @@ async function hillClimbing() {
     return min
   }
 
-  const solutions = await generateSolutions()
-  let bestSolution = {coutTotal: Infinity}
+  const solutions = await genererSolutions()
+  let meilleur = {coutTotal: Infinity}
 
   while (1) {
     const min = minSolutionsVoisines(solutions)
-    if (min.coutTotal >= bestSolution.coutTotal) 
-      return bestSolution
-    bestSolution = min
+    if (min.coutTotal >= meilleur.coutTotal) 
+      return meilleur
+    meilleur = min
   }
-  return bestSolution
+  return meilleur
 }
 
 async function simulatedAnnealing() {
   let temperature = 10000
-  const cool = 0.95
+  const refroidissement = 0.95
 
-  const solutions = await generateSolutions()
+  const solutions = await genererSolutions()
 
-  let highCost = solutions[0]
-  let lowCost = highCost
-  let bestSolution = lowCost
-  let maxStep = 10000
-  let step = 1
+  let haut = solutions[0]
+  let bas = haut
+  let meilleur = bas
+  let maxEtape = 10000
+  let etape = 1
 
-  while (step <= maxStep && temperature > 100) {
+  while (etape <= maxEtape && temperature > 100) {
 
-    let neighborIndex = randomIntFromInterval(0, solutions.length - 1)
-    if (solutions[neighborIndex].coutTotal < lowCost.coutTotal || Math.pow(Math.exp(1), ((-highCost.coutTotal-lowCost.coutTotal)/temperature)) > Math.random()) {
-      const temp = lowCost
-      lowCost = solutions[neighborIndex]
-      highCost = temp
-      temperature *= cool
-      bestSolution = lowCost
+    let neighborIndex = entierAleatoireEntre(0, solutions.length - 1)
+    if (solutions[neighborIndex].coutTotal < bas.coutTotal || Math.pow(Math.exp(1), ((-haut.coutTotal-bas.coutTotal)/temperature)) > Math.random()) {
+      const tmp = bas
+      bas = solutions[neighborIndex]
+      haut = tmp
+      temperature *= refroidissement
+      meilleur = bas
     }
 
-    step++
+    etape++
   }
 
-  return bestSolution
+  return meilleur
 }
+
+async function geneticAlgorithm() {
+
+}
+
 async function main() {
   let res1 = await hillClimbing()
   let res2 = await simulatedAnnealing()
+  console.log(res1)
+  console.log(res2)
   console.log('end')
+
 }
 
 main()
